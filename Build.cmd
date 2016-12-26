4c697ddc844a1b57add0a386c0a71eae28465582
Scott Baker <smbaker@gmail.com>
2016-12-25 16:42:33 -0800

    modify for smb-wd

--- Build.cmd
+++ Build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set TASMPATH=..\Tools\tasm32
+set TASMPATH=..\RomWBW\Tools\tasm32
 set TASMTABS=%TASMPATH%
 set PATH=%TASMPATH%;%PATH%
 
@@ -11,8 +11,12 @@ call :asm ZETA
 call :asm ZETA2
 call :asm DIDE
 call :asm N8
+call :asm SMB_WD
+copy FD_SMB_WD.COM ..\RomWBW\Source\RomDsk\smb_std\FD.COM
+copy FD_SMB_WD.COM ..\RomWBW\Source\RomDsk\smb_8inch\FD.COM
 goto :eof
 
 :asm
 TASM -t80 -b -g3 -fFF -dFDPLT=PLT_%1 fd.asm FD_%1.COM FD_%1.LST
 goto :eof
+
